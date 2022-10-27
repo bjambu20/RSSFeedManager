ecd50ab8fb9b217ca46677d56ca5ea87ce0b1ea3
bjambu20 <bjambu20@gmail.com>
2022-10-27 11:44:36 +0530

    changes done in Read Test
    changes done in Read Test

--- src/com/pack/rss/test/ReadTest.java
+++ src/com/pack/rss/test/ReadTest.java
@@ -1,60 +1,77 @@
 package com.pack.rss.test;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Optional;
 import com.pack.rss.model.Feed;
 import com.pack.rss.model.FeedMessage;
 import com.pack.rss.read.RSSFeedParser;
-import java.util.Properties;
-import javax.mail.*;
-import javax.mail.internet.*;
+
 public class ReadTest {
-	public static void main(String[] args) {
-		RSSFeedParser parser = new RSSFeedParser("https://cio.economictimes.indiatimes.com/rss/topstories");
-		Feed feed = parser.readFeed();
-		System.out.println(feed);
-		StringBuffer sb = new StringBuffer();
-		for (FeedMessage message : feed.getMessages()) {
-			sb.append(message);
-		}
-		System.out.println(sb.toString());
-		String feedsToBeSent = sb.toString();
-//      Also the system must send feeds to the users whenever an update receives from RSS API. 
-//		The system should not send feeds which are already sent. An efficient compression must be used by the system for transferring feeds to its users.
-//      Each news records send from System must have fields “News Header, News published date & News description”. 
-//		News header content size and Description content size will be dynamic 
-		String loggedInUser="bjambu20@gmail.com";//change accordingly
-		String from="flowerplus12345@gmail.com";//change accordingly
-		String password="*****";//change accordingly
 
-		//Get the session object
-		Properties props = new Properties();
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.socketFactory.port", "465");
-		props.put("mail.smtp.socketFactory.class",
-		"javax.net.ssl.SSLSocketFactory");
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.port", "465");
+	public static final String feedUrl = "https://cio.economictimes.indiatimes.com/rss/topstories";
+	public static Connection connection = null;
+	public static Statement statement = null;
 
-		Session session = Session.getDefaultInstance(props,
-		new javax.mail.Authenticator() {
-		protected PasswordAuthentication getPasswordAuthentication() {
-		return new PasswordAuthentication(from,password);
-		}
-		});
-
-		//compose message
+	public static void main(String[] args) throws SQLException {
 		try {
-		MimeMessage message = new MimeMessage(session);
-		message.setFrom(new InternetAddress(from));
-		message.addRecipient(Message.RecipientType.TO,new InternetAddress(loggedInUser));
-		message.setSubject("RSS FEED");
-		message.setText(feedsToBeSent);
+			RSSFeedParser parser = new RSSFeedParser(feedUrl);
+			Feed feed = parser.readFeed();
+			System.out.println(feed);
+			StringBuffer sb = new StringBuffer();
+			// get database connection
+			connection = DatabaseConnection.getConnection();
+			// get the statement
+			statement = connection.createStatement();
+			for (FeedMessage message : feed.getMessages()) {
+				// read the database record with last publish date
+				String query = "SELECT title,pubdate,description from RSSFEED order by pubdate desc limit 1";
+				ResultSet resultSet = statement.executeQuery(query);
+				Optional<ResultSet> optional = Optional.ofNullable(resultSet);
+				if (optional.isPresent()) {
+					while (resultSet.next()) {
+						String pubdate = resultSet.getString("pubdate");
+						SimpleDateFormat sf = new SimpleDateFormat("dd-MM-yyyy;HH:mm:ss");
+						try {
+							Date dbPubDateTime = sf.parse(pubdate);
+							System.out.println(dbPubDateTime);
+							Date pbDateTime = sf.parse(message.getPubdate());
+							// find if new rss feeds received from api
+							if (pbDateTime.compareTo(dbPubDateTime) > 0) {
+								// save rss feeds to database
+								String sql = "INSERT INTO RSSFEED VALUES (" + message.getTitle() + ", "
+										+ message.getPubdate() + ", " + message.getDescription() + ")";
+								statement.executeUpdate(sql);
+								sb.append(message);
+							}
+						} catch (ParseException e) {
+							e.printStackTrace();
+						}
+					}
+				} else {
+					sb.append(message);
+				}
+			}
+			System.out.println(sb.toString());
+			String feedsToBeSent = sb.toString();
 
-		//send message
-		Transport.send(message);
-
-		System.out.println("RSS Feed sent successfully");
-
-		} catch (MessagingException e) {throw new RuntimeException(e);}
-		
+			// if yes, send feeds to authenticated users.
+			// send rss feed over email
+			Mailer.send("flowerplus1234@gmail.com", "****", "bjambu20@gmail.com", "RSS Feed", feedsToBeSent); //change if needed
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (connection != null) {
+				connection.close();
+			}
+			if (statement != null) {
+				statement.close();
+			}
+		}
 	}
 }
