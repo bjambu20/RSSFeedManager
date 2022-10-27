e93a47f99af42481ee2990cba2bd07ac963955e8
bjambu20 <bjambu20@gmail.com>
2022-10-27 10:10:02 +0530

    Initial commit

---
+++ src/com/pack/rss/test/ReadTest.java
@@ -0,0 +1,68 @@
+package com.pack.rss.test;
+
+import com.pack.rss.model.Feed;
+import com.pack.rss.model.FeedMessage;
+import com.pack.rss.read.RSSFeedParser;
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
+public class ReadTest {
+	public static void main(String[] args) {
+		RSSFeedParser parser = new RSSFeedParser("https://cio.economictimes.indiatimes.com/rss/topstories");
+		Feed feed = parser.readFeed();
+		System.out.println(feed);
+		StringBuffer sb = new StringBuffer();
+		for (FeedMessage message : feed.getMessages()) {
+			sb.append(message);
+		}
+		System.out.println(sb.toString());
+		String feedsToBeSent = sb.toString();
+//        Design a RSS news feed Manager which should read and store the news from RSS
+
+//        API( http://cio.economictimes.indiatimes.com/rss/topstories). Manager must send the updated news to connected users who are authenticated.
+
+//        Also the system must send feeds to the users whenever an update receives from RSS API. 
+//		  The system should not send feeds which are already sent. An efficient compression must be used by the system for transferring feeds to its users.
+
+		String loggedInUser="bjambu20@gmail.com";//change accordingly
+		String from="flowerplus12345@gmail.com";//change accordingly
+		String password="*****";//change accordingly
+
+		//Get the session object
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.socketFactory.class",
+		"javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "465");
+
+		Session session = Session.getDefaultInstance(props,
+		new javax.mail.Authenticator() {
+		protected PasswordAuthentication getPasswordAuthentication() {
+		return new PasswordAuthentication(from,password);
+		}
+		});
+
+		//compose message
+		try {
+		MimeMessage message = new MimeMessage(session);
+		message.setFrom(new InternetAddress(from));
+		message.addRecipient(Message.RecipientType.TO,new InternetAddress(loggedInUser));
+		message.setSubject("RSS FEED");
+		message.setText(feedsToBeSent);
+
+		//send message
+		Transport.send(message);
+
+		System.out.println("RSS Feed sent successfully");
+
+		} catch (MessagingException e) {throw new RuntimeException(e);}
+		
+//        Each news records send from System must have fields “News Header, News published date & News description”. 
+//		  News header content size and Description content size will be dynamic 
+
+//        Details available in the project attachment. 
+		
+	}
+}
